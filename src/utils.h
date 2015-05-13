11fac9765a31f35fc58d7b359bedb314d5048518
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-05-13 17:40:41 -0400

    pragma

--- src/utils.h
+++ src/utils.h
@@ -321,21 +321,21 @@ static inline bool loadFromPath(const std::string& path, std::string* into) {
     if(!file.is_open()) return false;
     while(!file.eof()) {
         getline(file, buffer);
-	if(buffer.find("#include ") == 0){
-		unsigned begin = buffer.find_first_of("\"");
-		unsigned end = buffer.find_last_of("\"");
-		if(begin != end){
-			std::string file = buffer.substr(begin+1,end-begin-1);
-			std::string newBuffer;
-			if(loadFromPath(file,&newBuffer)){
-				(*into) += "\n" + newBuffer + "\n";
-			} else {
-				std::cout << file << " not found" << std::endl;
-			}
-		}
-	} else {
-        	(*into) += buffer + "\n";
-	}
+    	if(buffer.find("#include ") == 0 || buffer.find("#pragma include ") == 0){
+    		unsigned begin = buffer.find_first_of("\"");
+    		unsigned end = buffer.find_last_of("\"");
+    		if(begin != end){
+    			std::string file = buffer.substr(begin+1,end-begin-1);
+    			std::string newBuffer;
+    			if(loadFromPath(file,&newBuffer)){
+    				(*into) += "\n" + newBuffer + "\n";
+    			} else {
+    				std::cout << file << " not found" << std::endl;
+    			}
+    		}
+    	} else {
+            	(*into) += buffer + "\n";
+    	}
     }
 
     file.close();
