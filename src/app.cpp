ab41460104f6ecb44accf9205145198abc6c5a84
Doug Moen <doug@moens.org>
2017-02-25 11:51:02 -0500

    show informative error when glfwInit fails
    Here's an example:
    ```
    GLFW error 65544: X11: The DISPLAY environment variable is missing
    ABORT: GLFW init failed
    ```

--- src/app.cpp
+++ src/app.cpp
@@ -186,6 +186,9 @@ void initGL (glm::ivec4 &_viewport, bool _headless) {
     #else
         // OSX/LINUX use GLFW
         // ---------------------------------------------
+        glfwSetErrorCallback([](int err, const char* msg)->void {
+            std::cerr << "GLFW error "<<err<<": "<<msg<<"\n";
+        });
         if(!glfwInit()) {
             std::cerr << "ABORT: GLFW init failed" << std::endl;
             exit(-1);
