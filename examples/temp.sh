a9578e61c7e7a6c8bfa6d2837ee02a45abaf7859
Chris Hodapp <hodapp87@gmail.com>
2018-05-16 18:43:53 -0400

    Fixed non-working example (it ignores non-float inputs)
    u_temp is a float, but in both README.md and examples/temp.sh, it
    passes integers.  As the integers are ignored, the examples do not
    work.

--- examples/temp.sh
+++ examples/temp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-    awk '{print "u_temp,"$1/1000}' /sys/class/thermal/thermal_zone0/temp
+    awk '{print "u_temp,"$1/1000".0"}' /sys/class/thermal/thermal_zone0/temp
     sleep 1
 done
