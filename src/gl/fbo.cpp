e6f56905fe7aa6b0e544084a1289ece225524432
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2018-10-07 15:11:06 -0700

    no glDraw none

--- src/gl/fbo.cpp
+++ src/gl/fbo.cpp
@@ -134,10 +134,6 @@ void Fbo::allocate(const uint _width, const uint _height, FboType _type) {
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
             glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depth_texture, 0);
-
-            if (!color_texture) {
-                glDrawBuffer(GL_NONE);
-            }
         }
     }
 
