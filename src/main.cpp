790ee116ccfd10d406fa319f986008145cb0bf90
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2019-10-08 13:59:45 -0700

    fixing main for VC and GBM

--- src/main.cpp
+++ src/main.cpp
@@ -479,7 +479,7 @@ int main(int argc, char **argv){
 
     // Set the size
     glm::ivec4 windowPosAndSize = glm::ivec4(0);
-    #if defined(PLATFORM_RPI) || defined(PLATFORM_RPI4) 
+    #if defined(DRIVER_VC) || defined(DRIVER_GBM) 
         // RASPBERRYPI default windows size (fullscreen)
         glm::ivec2 screen = getScreenSize();
         windowPosAndSize.z = screen.x;
@@ -525,7 +525,7 @@ int main(int argc, char **argv){
         }
         else if (   std::string(argv[i]) == "-l" ||
                     std::string(argv[i]) == "--life-coding" ){
-        #if defined(PLATFORM_RPI) || defined(PLATFORM_RPI4) 
+        #if defined(DRIVER_VC) || defined(DRIVER_GBM) 
             windowPosAndSize.x = windowPosAndSize.z - 500;
             windowPosAndSize.z = windowPosAndSize.w = 500;
         #else
