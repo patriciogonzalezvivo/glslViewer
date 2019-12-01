275282ac064a028e5cb883e19288e405712c021e
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2019-12-01 11:31:59 -0800

    touchOSC emulator

--- examples/2D/07_touchOSC/touchOSC_emulator.py
+++ examples/2D/07_touchOSC/touchOSC_emulator.py
@@ -6,6 +6,12 @@ oscmsg = OSC.OSCMessage()
 oscmsg.setAddress("/multifaderM_1")
 oscmsg.append(0.5)
 
+oscmsg.setAddress("/multifaderM_2")
+oscmsg.append(0.25)
+
+oscmsg.setAddress("/multifaderM_3")
+oscmsg.append(0.75)
+
 # oscmsg.setAddress("/debug")
 # oscmsg.append('on')
 
