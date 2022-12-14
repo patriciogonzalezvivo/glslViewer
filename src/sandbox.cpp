bbe7808512024c2c93ac6ea433a4ecdd310ae147
tcoyvwac <53616399+tcoyvwac@users.noreply.github.com>
2022-12-14 18:53:17 +0100

    generalise code using helper lambdas and tuples
    * Added tuple for plot_value display
    * Added helper lambda function-objects

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -313,35 +313,33 @@ void Sandbox::commandsInit(CommandList &_commands ) {
                         values[1] = "off";
                 }
 
-                if (values[1] == "off") 
-                    m_plot = PLOT_OFF;
-                else if (values[1] == "luma") {
-                    m_plot = PLOT_LUMA;
-                     m_plot_shader.addDefine("PLOT_VALUE", "color.rgb = vec3(step(st.y, data.a)); color += stroke(fract(st.x * 5.0), 0.5, 0.025) * 0.1;");
-                }
-                else if (values[1] == "red") {
-                    m_plot = PLOT_RED;
-                     m_plot_shader.addDefine("PLOT_VALUE", "color.rgb = vec3(step(st.y, data.r), 0.0, 0.0);  color += stroke(fract(st.x * 5.0), 0.5, 0.025) * 0.1;");
-                }
-                else if (values[1] == "green") {
-                    m_plot = PLOT_GREEN;
-                    m_plot_shader.addDefine("PLOT_VALUE", "color.rgb = vec3(0.0, step(st.y, data.g), 0.0);  color += stroke(fract(st.x * 5.0), 0.5, 0.025) * 0.1;");
-                }
-                else if (values[1] == "blue") {
-                    m_plot = PLOT_BLUE;
-                    m_plot_shader.addDefine("PLOT_VALUE", "color.rgb = vec3(0.0, 0.0, step(st.y, data.b));  color += stroke(fract(st.x * 5.0), 0.5, 0.025) * 0.1;");
-                }
-                else if (values[1] == "rgb") {
-                    m_plot = PLOT_RGB;
-                    m_plot_shader.addDefine("PLOT_VALUE", "color += stroke(fract(st.x * 5.0), 0.5, 0.025) * 0.1;");
-                }
-                else if (values[1] == "fps") {
-                    m_plot = PLOT_FPS;
-                    m_plot_shader.addDefine("PLOT_VALUE", "color.rgb += digits(uv * 0.1 + vec2(0.0, -0.01), value.r * 60.0, 1.0); color += stroke(fract(st.y * 3.0), 0.5, 0.05) * 0.1;");
-                }
-                else if (values[1] == "ms") {
-                    m_plot = PLOT_MS;
-                    m_plot_shader.addDefine("PLOT_VALUE", "color.rgb += digits(uv * 0.1 + vec2(0.105, -0.01), value.r * 60.0, 1.0); color += stroke(fract(st.y * 3.0), 0.5, 0.05) * 0.1;");
+                using polt_value_display_t = std::tuple<std::string, PlotType, std::string>;
+                const std::vector<polt_value_display_t> lala = {
+                    {"luma", PLOT_LUMA, "color.rgb = vec3(step(st.y, data.a)); color += stroke(fract(st.x * 5.0), 0.5, 0.025) * 0.1;"}
+                    , {"red", PLOT_RED, "color.rgb = vec3(step(st.y, data.r), 0.0, 0.0);  color += stroke(fract(st.x * 5.0), 0.5, 0.025) * 0.1;"}
+                    , {"green", PLOT_GREEN, "color.rgb = vec3(0.0, step(st.y, data.g), 0.0);  color += stroke(fract(st.x * 5.0), 0.5, 0.025) * 0.1;"}
+                    , {"blue", PLOT_BLUE, "color.rgb = vec3(0.0, 0.0, step(st.y, data.b));  color += stroke(fract(st.x * 5.0), 0.5, 0.025) * 0.1;"}
+                    , {"rgb", PLOT_RGB, "color += stroke(fract(st.x * 5.0), 0.5, 0.025) * 0.1;"}
+                    , {"fps", PLOT_FPS,"color.rgb += digits(uv * 0.1 + vec2(0.0, -0.01), value.r * 60.0, 1.0); color += stroke(fract(st.y * 3.0), 0.5, 0.05) * 0.1;"}
+                    , {"ms", PLOT_MS, "color.rgb += digits(uv * 0.1 + vec2(0.105, -0.01), value.r * 60.0, 1.0); color += stroke(fract(st.y * 3.0), 0.5, 0.05) * 0.1;"}
+                };
+
+                const auto switch_off = [&](){ m_plot = PLOT_OFF;};
+                const auto set_appropriate_plot = [&](const std::string plot_id_keyword ){
+                    const auto it = std::find_if(std::begin(lala), std::end(lala)
+                                                 , [&](const polt_value_display_t& i){return std::get<0>(i) == plot_id_keyword;});
+                    if(it != std::end(lala)) {
+                        const auto define_text_primary_key = "PLOT_VALUE";
+                        m_plot = std::get<1>(*it);
+                        m_plot_shader.addDefine(define_text_primary_key, std::get<2>(*it));
+                    }
+                };
+
+                const auto plot_id_keyword = values[1];
+                if (plot_id_keyword == "off") {
+                    switch_off();
+                } else {
+                    set_appropriate_plot(plot_id_keyword);
                 }
                 m_update_buffers = true;
                 return true;
