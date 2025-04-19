4563086c53099051bb466df0f1a80b8200234093
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2025-04-19 07:21:19 -0400

    artfactory 3

--- docs/glslViewer.js
+++ docs/glslViewer.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp78_a_cvp.js
+// include: /tmp/tmp9exi6r79.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -214,21 +214,21 @@ Module['FS_createPath']("/examples/io", "frames", true, true);
 
   })();
 
-// end include: /tmp/tmp78_a_cvp.js
-// include: /tmp/tmp17u7vrk0.js
+// end include: /tmp/tmp9exi6r79.js
+// include: /tmp/tmp66k0fhs8.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp17u7vrk0.js
-// include: /tmp/tmp5cc6j30r.js
+  // end include: /tmp/tmp66k0fhs8.js
+// include: /tmp/tmpyfmszr32.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp5cc6j30r.js
+  // end include: /tmp/tmpyfmszr32.js
 
 
 // Sometimes an existing Module object exists with properties
